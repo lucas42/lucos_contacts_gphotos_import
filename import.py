bee9e8261b83557c73e705f1e6c99ca1c5a632bd
Luke Blaney <git@lukeblaney.co.uk>
2022-11-10 00:15:25 +0000

    use new importer functionality in lucos_contacts to simplify logic here

--- import.py
+++ import.py
@@ -5,28 +5,6 @@ from bs4 import BeautifulSoup
 CONTACTS_URL = os.environ.get('LUCOS_CONTACTS', "https://contacts.l42.eu/")
 LUCOS_HEADERS={'AUTHORIZATION':"key "+os.environ.get('LUCOS_CONTACTS_API_KEY')}
 
-
-# Search for an existing match in lucos, based on the order of items in the accounts arround
-#
-# Returns the agentid as a string, if a match is found.  Otherwise returns None
-def matchContact(accounts, primaryName):
-	for account in accounts:
-		resp = requests.get(CONTACTS_URL+"identify", headers=LUCOS_HEADERS, params=account, allow_redirects=False)
-		if resp.status_code == 302:
-			return resp.headers['Location'].replace("/agents/","")
-		if resp.status_code == 409:
-			print("Conflict for "+primaryName+" - "+account['type'])
-		if resp.status_code >= 500:
-			resp.raise_for_status()
-	return None
-
-def newContact(name):
-	resp = requests.post(CONTACTS_URL+"agents/add", headers=LUCOS_HEADERS, allow_redirects=False, data={'name': name})
-	if resp.status_code == 302:
-		return resp.headers['Location'].replace("/agents/","")
-	raise Exception("Unexpected status code "+str(resp.status_code)+" "+resp.reason+": "+resp.text)
-
-
 with open('data.html') as fp:
 	doc = BeautifulSoup(fp, "html.parser")
 	for script in doc.find_all("script"):
@@ -40,9 +18,24 @@ with open('data.html') as fp:
 			name = row[1]
 			if not name:
 				continue
-			image = row[2]
-			numericId = row[3]
-			id1 = row[8]
-			id2 = row[11] # All examples I've looked at have the same value for id1 and id2
-			url = f"/_search/c{id1}_{name}"
-			print(name, url)
+			profile_pic_url = row[2]
+			person_id = row[3]
+			cluster_media_key = row[8] # Same as row[11] from what I can tell
+			escaped_key = cluster_media_key.replace("_", "~u")
+			search_path = f"/search/_c{escaped_key}_{name}"
+
+			data = {"identifiers": [
+				{
+					"type":"googlephotos",
+					"person_id": person_id,
+					"cluster_media_key": cluster_media_key,
+					"search_path": search_path,
+					"profile_pic_url": profile_pic_url,
+				},
+				{
+					"type":"name",
+					"name": name,
+				}
+			]};
+			resp = requests.post(CONTACTS_URL+'agents/import', headers=LUCOS_HEADERS, allow_redirects=False, json=data)
+			resp.raise_for_status()
